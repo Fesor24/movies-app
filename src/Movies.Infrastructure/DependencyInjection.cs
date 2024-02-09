d4db4a158b7b450e634ac44d85ebb9e0df1b69b5
Wale <onafesowale@gmail.com>
2024-02-09 15:27:17 +0100

    dependency injection

---
+++ src/Movies.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Domain.Services.Common;
+using Movies.Infrastructure.Services.Common;
+
+namespace Movies.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddHttpClient();
+
+        services.AddScoped<IHttpClient, CustomHttpClient>();
+
+        return services;
+    }
+}
