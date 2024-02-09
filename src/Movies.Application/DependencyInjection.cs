177eee9235f2525ad7d5e1256b55756fd1b855fb
Wale <onafesowale@gmail.com>
2024-02-09 22:19:31 +0100

    dependency injection

---
+++ src/Movies.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Features.Movie.Queries.Search;
+using System.Reflection;
+
+namespace Movies.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssemblyContaining<SearchMovieRequest>();
+        });
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
