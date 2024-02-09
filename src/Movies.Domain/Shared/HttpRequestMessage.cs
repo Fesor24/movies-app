250df729fae20cd86e4cab40b8366dc8f23e380e
Wale <onafesowale@gmail.com>
2024-02-09 15:27:09 +0100

    HttpRequestMessage

---
+++ src/Movies.Domain/Shared/HttpRequestMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.Domain.Shared;
+public class HttpRequestMessage
+{
+    public string Uri { get; set; }
+    public Dictionary<string, string> Headers { get; set; } = new();
+    public HttpMethod Method { get; set; }
+}
+
+public class HttpRequestMessage<TBody> : HttpRequestMessage
+{
+    public TBody Body { get; set; }
+}
+
+
