573f05011cfa9471be41c1c8d91c8dfec186c3a0
Wale <onafesowale@gmail.com>
2024-02-09 15:26:41 +0100

    result generic

---
+++ src/Movies.Domain/Primitives/ResultT.cs
@@ -0,0 +1,26 @@
+﻿namespace Movies.Domain.Primitives;
+public class Result<TValue, TError> : Result where TError : Error
+{
+    private readonly TValue _value;
+    private readonly TError _error;
+
+    public Result(TValue value) : base()
+    {
+        _value = value;
+    }
+
+    public Result(TError error) : base(false, error)
+    {
+        _error = error;
+    }
+
+    public TValue Value => IsSuccess ? 
+        _value : 
+        throw new Exception("Value can not be accessed");
+
+    public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+    public static implicit operator Result<TValue, TError>(TError error) => new(error);
+
+    public TResult Match<TResult>(Func<TValue, TResult> success, Func<TError, TResult> failure) =>
+        IsSuccess ? success(_value) : failure(_error);
+}
