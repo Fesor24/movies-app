34ad644d275754208f73dde28f06832e3a33a4da
Wale <onafesowale@gmail.com>
2024-02-09 15:25:13 +0100

    register services

--- src/Movies.API/Program.cs
+++ src/Movies.API/Program.cs
@@ -1,4 +1,13 @@
+using Movies.Domain.Models;
+using Movies.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureServices();
+
+builder.Services.Configure<ImdbCredentials>(
+    builder.Configuration.GetSection(ImdbCredentials.CONFIGURATION));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
