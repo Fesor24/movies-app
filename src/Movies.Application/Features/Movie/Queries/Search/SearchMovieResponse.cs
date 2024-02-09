a20a9c72c7f574fa4f8339303b2a0befdb20ce25
Wale <onafesowale@gmail.com>
2024-02-09 22:19:01 +0100

    search movie response

---
+++ src/Movies.Application/Features/Movie/Queries/Search/SearchMovieResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Movies.Application.Features.Movie.Queries.Search;
+public class SearchMovieResponse
+{
+    public int TotalRecords { get; set; }
+    public List<MovieResponse> Movies { get; set; }
+
+}
+
+public class MovieResponse
+{
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string ImdbId { get; set; }
+    public string Type { get; set; }
+    public string Poster { get; set; }
+}
