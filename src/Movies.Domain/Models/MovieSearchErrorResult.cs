8b931c65d7d5af7fb2283407fc05659b2b0cc400
Wale <onafesowale@gmail.com>
2024-02-09 15:25:49 +0100

    movie search error result

---
+++ src/Movies.Domain/Models/MovieSearchErrorResult.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Domain.Primitives;
+
+namespace Movies.Domain.Models;
+public class MovieSearchErrorResult : Error
+{
+    public MovieSearchErrorResult(string code, string message) : base(code, message)
+    {
+        
+    }
+
+    public MovieSearchErrorResult()
+    {
+        
+    }
+
+    public string Response { get; set; }
+    public string Error { get; set; }
+}
+
