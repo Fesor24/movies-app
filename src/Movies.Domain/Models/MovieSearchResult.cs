4dfce699ba72d70a617333ff6a7a305254129ba3
Wale <onafesowale@gmail.com>
2024-02-09 22:18:33 +0100

    json property naming

--- src/Movies.Domain/Models/MovieSearchResult.cs
+++ src/Movies.Domain/Models/MovieSearchResult.cs
@@ -1,6 +1,9 @@
-﻿namespace Movies.Domain.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Movies.Domain.Models;
 public class MovieSearchResult
 {
+    [JsonPropertyName("totalResults")]
     public string TotalResults { get; set; }
     public string Response { get; set; }
     public List<Movie> Search { get; set; }
@@ -10,6 +13,7 @@ public class Movie
 {
     public string Title { get; set; }
     public string Year { get; set; }
+    [JsonPropertyName("imdbID")]
     public string ImdbId { get; set; }
     public string Type { get; set; }
     public string Poster { get; set; }
