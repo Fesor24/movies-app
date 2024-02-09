a0957cc6a1c0d5125b8ee79aeae6afe1785f66dc
Wale <onafesowale@gmail.com>
2024-02-09 15:26:22 +0100

    error

---
+++ src/Movies.Domain/Primitives/Error.cs
@@ -0,0 +1,28 @@
+﻿namespace Movies.Domain.Primitives;
+public class Error
+{
+    public Error()
+    {
+        
+    }
+
+    public Error(string code, string message, string details)
+    {
+        Code = code;
+        Message = message;
+        Details = details;
+    }
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+        Details = string.Empty;
+    }
+
+    public static Error None => new(string.Empty, string.Empty, string.Empty);
+
+    public string Code { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
+}
