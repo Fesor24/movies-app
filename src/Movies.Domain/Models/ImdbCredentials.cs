dbf73acea905dd0b44ffc2c3c205781f00dd2efa
Wale <onafesowale@gmail.com>
2024-02-09 15:25:29 +0100

    Imdb creds

---
+++ src/Movies.Domain/Models/ImdbCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace Movies.Domain.Models;
+public class ImdbCredentials
+{
+    public static readonly string CONFIGURATION = "Imdb";
+    public string BaseUrl { get; set; }
+    public string ApiKey { get; set; }
+}
