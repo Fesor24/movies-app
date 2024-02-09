9c2d3d20594fd17dd6d914b4eedb7cc7cb64a183
Wale <onafesowale@gmail.com>
2024-02-09 22:18:48 +0100

    movie mappings

---
+++ src/Movies.Application/Mappings/MovieMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Movies.Application.Features.Movie.Queries.Search;
+using Movies.Domain.Models;
+
+namespace Movies.Application.Mappings;
+internal class MovieMapping : Profile
+{
+    public MovieMapping()
+    {
+        CreateMap<Movie, MovieResponse>();
+    }
+}
