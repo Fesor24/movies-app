abe728377677f5686e1bdea17af560cfe9d7c2af
Wale <onafesowale@gmail.com>
2024-02-09 22:17:08 +0100

    movies endpoint

---
+++ src/Movies.API/Endpoints/MoviesEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Movies.API.Abstractions;
+using Movies.API.Extensions;
+using Movies.Application.Features.Movie.Queries.Search;
+
+namespace Movies.API.Endpoints;
+
+public class MoviesEndpoint : IEndpointDefinition
+{
+    public void RegisterEndpoint(WebApplication app)
+    {
+        const string ENDPOINT = "movies";
+
+        app.MediatorGet<SearchMovieRequest, SearchMovieResponse>(ENDPOINT, "/search");
+    }
+}
