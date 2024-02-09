2b62655b7ac670ecf933280d3d658f43c20c2bc2
Wale <onafesowale@gmail.com>
2024-02-09 15:26:58 +0100

    IMovie Service

---
+++ src/Movies.Domain/Services/IMovieService.cs
@@ -0,0 +1,8 @@
+﻿using Movies.Domain.Models;
+using Movies.Domain.Primitives;
+
+namespace Movies.Domain.Services;
+public interface IMovieService
+{
+    Task<Result<MovieSearchResult, Error>> Search(string searchTerm);
+}
