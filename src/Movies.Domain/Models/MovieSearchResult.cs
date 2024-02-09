a0271ef90ac5a07edc7602af262714694a0cae3a
Wale <onafesowale@gmail.com>
2024-02-09 15:26:03 +0100

    movie search result

---
+++ src/Movies.Domain/Models/MovieSearchResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Movies.Domain.Models;
+public class MovieSearchResult
+{
+    public string TotalResults { get; set; }
+    public string Response { get; set; }
+    public List<Movie> Search { get; set; }
+}
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string ImdbId { get; set; }
+    public string Type { get; set; }
+    public string Poster { get; set; }
+}
