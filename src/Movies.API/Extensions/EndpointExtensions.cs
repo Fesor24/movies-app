f307adb5115aedd43c857e8e335bc163b8253b0a
Wale <onafesowale@gmail.com>
2024-02-09 22:17:20 +0100

    endpoint extensions

---
+++ src/Movies.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Movies.API.Abstractions;
+using System.Reflection;
+
+namespace Movies.API.Extensions;
+
+public static class EndpointExtensions
+{
+    public static void RegisterEndpoints(this WebApplication app)
+    {
+        var endpoints = Assembly.GetExecutingAssembly()
+            .GetExportedTypes()
+            .Where(x => x.IsAssignableTo(typeof(IEndpointDefinition)) && !x.IsAbstract && !x.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpointDefinition>();
+
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.RegisterEndpoint(app);
+        }
+    }
+}
