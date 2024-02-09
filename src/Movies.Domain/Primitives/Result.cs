73c602440b4cf12d35e25be9c42cc45db64b941e
Wale <onafesowale@gmail.com>
2024-02-09 15:26:33 +0100

    result

---
+++ src/Movies.Domain/Primitives/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace Movies.Domain.Primitives;
+public class Result
+{
+    protected Result()
+    {
+        IsSuccess = true;
+        //Error = Error.None;
+    }
+
+    protected Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; private set; }
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; private set; }
+
+    public static Result Success => new();
+    public static Result Failure(Error error) => new(false, error);
+}
