7752a57044684719b605a35ff3270eba0d635d9c
Wale <onafesowale@gmail.com>
2024-02-10 20:50:30 +0100

    exception middleware

---
+++ src/Movies.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Movies.Domain.Primitives;
+using System.Net;
+using System.Text.Json;
+
+namespace Movies.API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env)
+    {
+        _next = next;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var error = _env.IsDevelopment() ?
+                new Error("500", ex.Message, ex.StackTrace) :
+                new Error("500", ex.Message);
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            await context.Response
+                .WriteAsync(JsonSerializer.Serialize(error, jsonOptions));
+
+        }
+    }
+}
