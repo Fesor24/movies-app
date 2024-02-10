39fe58475cb28546b4e2a9873fa37d000abc8cde
Wale <onafesowale@gmail.com>
2024-02-10 03:08:06 +0100

    get movies request

---
+++ src/Movies.Application/Features/Movie/Queries/GetByImdbId/GetMovieByImdbIdRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using Movies.Domain.Primitives;
+
+namespace Movies.Application.Features.Movie.Queries.GetByImdbId;
+public record GetMovieByImdbIdRequest(string ImdbId) : IRequest<Result<GetMovieResponse, Error>>;
