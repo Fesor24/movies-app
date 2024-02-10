ebfe88674c4dd5107d9dee386fbee6b8b5b18a0d
Wale <onafesowale@gmail.com>
2024-02-10 03:08:39 +0100

    added mappings

--- src/Movies.Application/Mappings/MovieMapping.cs
+++ src/Movies.Application/Mappings/MovieMapping.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
+using Movies.Application.Features.Movie.Queries.GetByImdbId;
 using Movies.Application.Features.Movie.Queries.Search;
 using Movies.Domain.Models;
 
 namespace Movies.Application.Mappings;
-internal class MovieMapping : Profile
+public class MovieMapping : Profile
 {
     public MovieMapping()
     {
         CreateMap<Movie, MovieResponse>();
+        CreateMap<MovieItemSearchResult, GetMovieResponse>();
+        CreateMap<Ratings, MovieRating>();
     }
 }
