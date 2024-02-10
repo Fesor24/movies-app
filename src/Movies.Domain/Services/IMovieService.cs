a312ff248a8697e59b2ba240e864ef2002301ed4
Wale <onafesowale@gmail.com>
2024-02-10 20:53:01 +0100

    added page

--- src/Movies.Domain/Services/IMovieService.cs
+++ src/Movies.Domain/Services/IMovieService.cs
@@ -4,6 +4,6 @@ using Movies.Domain.Primitives;
 namespace Movies.Domain.Services;
 public interface IMovieService
 {
-    Task<Result<MovieSearchResult, Error>> Search(string searchTerm);
+    Task<Result<MovieSearchResult, Error>> Search(string searchTerm, int page);
     Task<Result<MovieItemSearchResult, Error>> GetMovieByImdbId(string imdbId);
 }
