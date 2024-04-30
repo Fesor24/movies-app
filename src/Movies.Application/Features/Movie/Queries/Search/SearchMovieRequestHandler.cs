a97e3904e930a1c2652576d047cb7dcb81f45b56
Wale <onafesowale@gmail.com>
2024-04-30 05:02:37 +0100

    replace error message

--- src/Movies.Application/Features/Movie/Queries/Search/SearchMovieRequestHandler.cs
+++ src/Movies.Application/Features/Movie/Queries/Search/SearchMovieRequestHandler.cs
@@ -1,14 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
+using Movies.Application.Services;
 using Movies.Domain.Primitives;
-using Movies.Domain.Services;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleToAttribute("Movies.UnitTests")]
 
 namespace Movies.Application.Features.Movie.Queries.Search;
 internal sealed class SearchMovieRequestHandler : 
-    IRequestHandler<SearchMovieRequest, Result<SearchMovieResponse, Error>>
+    IRequestHandler<SearchMovieRequest, Result<SearchMovieResponse>>
 {
     private readonly IMovieService _movieService;
     private readonly IMapper _mapper;
@@ -19,12 +16,12 @@ internal sealed class SearchMovieRequestHandler :
         _mapper = mapper;
     }
 
-    public async Task<Result<SearchMovieResponse, Error>> Handle(SearchMovieRequest request, 
+    public async Task<Result<SearchMovieResponse>> Handle(SearchMovieRequest request, 
         CancellationToken cancellationToken)
     {
         if (string.IsNullOrWhiteSpace(request.SearchTerm))
-            return new Error("404", "Empty or null string passed as search term");
-
+            return new ValidationError("MOVIE_SEARCH_TERM", "Search term can not be null or empty");
+            
         var res = await _movieService.Search(request.SearchTerm, request.Page);
 
         if (res.IsFailure)
