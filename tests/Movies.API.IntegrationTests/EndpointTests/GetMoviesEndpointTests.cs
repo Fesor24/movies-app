7c7246d9647dd6dbcb0af2e1d6865dbe0bb41c74
Wale <onafesowale@gmail.com>
2024-04-30 09:23:43 +0100

    get movies endpoint

---
+++ tests/Movies.API.IntegrationTests/EndpointTests/GetMoviesEndpointTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Movies.API.IntegrationTests.Fixtures;
+using Movies.Application.Features.Movie.Queries.Search;
+using System.Net.Http.Json;
+
+namespace Movies.API.IntegrationTests.EndpointTests;
+public class GetMoviesEndpointTests : IClassFixture<MovieApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public GetMoviesEndpointTests(MovieApplicationFactory appFactory)
+    {
+        _httpClient = appFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task SearchMovies_WithValidSearchTerm_ReturnsListOfMovies()
+    {
+        string url = $"/api/movies/search?searchTerm=superman&page=1";
+
+        var response = await _httpClient.GetAsync(url);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var result = await response.Content.ReadFromJsonAsync<SearchMovieResponse>();
+
+        result.Should().NotBeNull();
+
+        result!.TotalRecords.Should().BeGreaterThan(1);
+
+        result!.Movies.Count.Should().Be(result.TotalRecords);
+
+        result!.Movies.ForEach(movie => movie.Title.Contains("superman"));
+    }
+}
