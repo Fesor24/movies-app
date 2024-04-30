8578360f3c665f7afef65766d5406aca24d275e4
Wale <onafesowale@gmail.com>
2024-04-30 05:04:12 +0100

    removed class

--- src/Movies.Domain/Services/IMovieService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Movies.Domain.Models;
-using Movies.Domain.Primitives;
-
-namespace Movies.Domain.Services;
-public interface IMovieService
-{
-    Task<Result<MovieSearchResult, Error>> Search(string searchTerm, int page);
-    Task<Result<MovieItemSearchResult, Error>> GetMovieByImdbId(string imdbId);
-}
