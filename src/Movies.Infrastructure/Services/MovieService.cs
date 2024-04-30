b4038e5ae873d096054b50c54cd10b6646182cf9
Wale <onafesowale@gmail.com>
2024-04-30 05:05:14 +0100

    modified movie service

--- src/Movies.Infrastructure/Services/MovieService.cs
+++ src/Movies.Infrastructure/Services/MovieService.cs
@@ -1,81 +1,58 @@
 ﻿using Microsoft.Extensions.Options;
+using Movies.Application.Services;
 using Movies.Domain.Models;
 using Movies.Domain.Primitives;
-using Movies.Domain.Services;
-using Movies.Domain.Services.Common;
+using System.Net.Http.Json;
 
 namespace Movies.Infrastructure.Services;
 public class MovieService : IMovieService
 {
-    private readonly IHttpClient _httpClient;
-    private readonly ImdbCredentials _credentials;
-    private string _url;
+    private readonly HttpClient _httpClient;
+    private readonly string _url;
 
-    public MovieService(IHttpClient httpClient, IOptions<ImdbCredentials> credentials)
+    public MovieService(HttpClient httpClient, IOptions<ImdbCredentials> credentials)
     {
         _httpClient = httpClient;
-        _credentials = credentials.Value;
-        _url = credentials.Value.BaseUrl + $"?apiKey={credentials.Value.ApiKey}";
+        _url = httpClient.BaseAddress + $"?apiKey={credentials.Value.ApiKey}";
     }
 
-    public async Task<Result<MovieItemSearchResult, Error>> GetMovieByImdbId(string imdbId)
+    public async Task<Result<MovieItemSearchResult>> GetMovieByImdbId(string imdbId)
     {
-        _url += $"&i={imdbId}";
+        string url = _url + $"&i={imdbId}";
 
-        var response = await _httpClient.SendAsync<MovieItemSearchResult, MovieSearchErrorResult>(
-            new Domain.Shared.HttpRequestMessage
-            {
-                Headers = new Dictionary<string, string>(),
-                Method = HttpMethod.Get,
-                Uri = _url
-            });
+        var response = await _httpClient.GetAsync(url);
 
-        if (response.IsSuccess && response.Value.Response == "True") return response.Value;
-
-        else
+        if (!response.IsSuccessStatusCode)
         {
-            if (response.IsSuccess)
-                return new Error(string.Empty, "An error occurred while getting this movie",
-                    "Possible reasons: Incorrect Imdb Id");
+            var errorMessage = response.ReasonPhrase;
 
-            if (response.Error is MovieSearchErrorResult errorResult)
-            {
-                Error err = new(string.Empty, errorResult.Message, errorResult.Error ??= string.Empty);
+            var errorDetails = await response.Content.ReadAsStringAsync();
 
-                return err;
-            }
-
-            return response.Error;
+            return new Error("GET_MOVIE_BY_ID", $"{errorMessage} \n {errorDetails}");
         }
+
+        var movie = await response.Content.ReadFromJsonAsync<MovieItemSearchResult>();
+
+        return movie;
     }
 
-    public async Task<Result<MovieSearchResult, Error>> Search(string searchTerm, int page)
+    public async Task<Result<MovieSearchResult>> Search(string searchTerm, int page)
     {
-        _url += $"&s={searchTerm}&page={page}";
+        string url = _url + $"&s={searchTerm}&page={page}";
 
-        var response = await _httpClient.SendAsync<MovieSearchResult, MovieSearchErrorResult>
-            (new Domain.Shared.HttpRequestMessage
+        var response = await _httpClient.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
         {
-            Headers = new Dictionary<string, string>(),
-            Method = HttpMethod.Get,
-            Uri = _url
-        });
+            var errorMessage = response.ReasonPhrase;
 
-        if (response.IsSuccess && response.Value.Response == "True") return response.Value;
+            var errorDetails = await response.Content.ReadAsStringAsync();
 
-        else
-        {
-            if (response.IsSuccess)
-                return new Error(string.Empty, "An error occurred while searching for this movie");
-
-            if(response.Error is MovieSearchErrorResult errorResult)
-            {
-                Error err = new(string.Empty, errorResult.Message, errorResult.Error ??= string.Empty);
-
-                return err;
-            }
-
-            return response.Error;
+            return new Error("GET_MOVIE_BY_ID", $"{errorMessage} \n {errorDetails}");
         }
+
+        var movies = await response.Content.ReadFromJsonAsync<MovieSearchResult>();
+
+        return movies;
     }
 }
