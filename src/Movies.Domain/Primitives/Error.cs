93da1f7bf25c175e91e59d18d9aded42f464ce9f
Wale <onafesowale@gmail.com>
2024-04-30 05:03:25 +0100

    removed details prop

--- src/Movies.Domain/Primitives/Error.cs
+++ src/Movies.Domain/Primitives/Error.cs
@@ -6,23 +6,38 @@ public class Error
         
     }
 
-    public Error(string code, string message, string details)
-    {
-        Code = code;
-        Message = message;
-        Details = details;
-    }
-
     public Error(string code, string message)
     {
         Code = code;
         Message = message;
-        Details = string.Empty;
     }
 
-    public static Error None => new(string.Empty, string.Empty, string.Empty);
+    public static Error None => new(string.Empty, string.Empty);
 
     public string Code { get; set; }
     public string Message { get; set; }
-    public string Details { get; set; }
+}
+
+public class ValidationError : Error
+{
+    public ValidationError(string code, string message) : base(code, message)
+    {
+        
+    }
+}
+
+public class NotFoundError : Error
+{
+    public NotFoundError(string code, string message) : base(code, message)
+    {
+
+    }
+}
+
+public class BadRequest : Error
+{
+    public BadRequest(string code, string message) : base(code, message)
+    {
+
+    }
 }
