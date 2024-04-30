398e5bdc71a9c3ac50fbfff46e7de9426fc36c11
Wale <onafesowale@gmail.com>
2024-04-30 05:03:03 +0100

    IMovieService

---
+++ src/Movies.Application/Services/IMovieService.cs
@@ -0,0 +1,10 @@
+﻿using Movies.Domain.Models;
+using Movies.Domain.Primitives;
+
+namespace Movies.Application.Services;
+public interface IMovieService
+{
+    Task<Result<MovieItemSearchResult>> GetMovieByImdbId(string id);
+
+    Task<Result<MovieSearchResult>> Search(string searchTerm, int page);
+}
