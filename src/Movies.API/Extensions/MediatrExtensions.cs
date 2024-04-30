3484d5325189f3055df70972f6360c2b433faa35
Wale <onafesowale@gmail.com>
2024-04-30 05:00:57 +0100

    handle error method

--- src/Movies.API/Extensions/MediatrExtensions.cs
+++ src/Movies.API/Extensions/MediatrExtensions.cs
@@ -6,7 +6,7 @@ namespace Movies.API.Extensions;
 internal static class MediatrExtensions
 {
     internal static void MediatorGet<TRequest, TResponse>(this WebApplication app, string endpointGroup, string route)
-        where TRequest : IRequest<Result<TResponse, Error>>
+        where TRequest : IRequest<Result<TResponse>>
     {
         route = "api/" + endpointGroup + route;
 
@@ -16,10 +16,20 @@ internal static class MediatrExtensions
 
     private static async Task<IResult> HandleGetRequests<TRequest, TResponse>([AsParameters] TRequest request, 
         ISender sender)
-        where TRequest : IRequest<Result<TResponse, Error>>
+        where TRequest : IRequest<Result<TResponse>>
     {
         var res = await sender.Send(request);
 
-        return res.Match(res => Results.Ok(res), err => Results.BadRequest(err));
+        return res.Match(res => Results.Ok(res), HandleError);
+    }
+
+    internal static IResult HandleError(Error error)
+    {
+        if (error.GetType() == typeof(NotFoundError))
+            return Results.NotFound(error);
+        else if (error.GetType() == typeof(ValidationError))
+            return Results.BadRequest(error);
+        else
+            return Results.BadRequest(error);
     }
 }
